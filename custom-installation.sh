4feffde91d7b14eec98f13678e3f91a05ab0397a
Sarthak Goel <goel.sar@husky.neu.edu>
2020-03-18 01:46:56 -0400

    Refactoring the code

--- custom-installation.sh
+++ custom-installation.sh
@@ -113,7 +113,6 @@ cd ~
 #Installing Cloud watch agent
 sudo add-apt-repository universe
 sudo apt-get update
-sudo apt-get install alien -y
 
 echo 'Downloading cloud watch'
 wget https://s3.amazonaws.com/amazoncloudwatch-agent/ubuntu/amd64/latest/amazon-cloudwatch-agent.deb
